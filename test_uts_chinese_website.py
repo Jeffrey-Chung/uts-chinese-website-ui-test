a6aaec0feac51456a9bd43cd40142d7561c94f35
Jeffrey-Chung <Cheuk.Chung@uts.edu.au>
2023-03-24 15:22:24 +1100

    Removed finding left and right arrow keys at first
    See comments on test_uts_chinese_website.py

--- test_uts_chinese_website.py
+++ test_uts_chinese_website.py
@@ -50,6 +50,7 @@ def ui_test(driver):
         else:
                 print("The website does not have a valid certificate.")
         action_chain = ActionChains(driver)
+        '''For whatever reason, github action is not able to find the right and left arrow key element'''
         #right_arrow_key = driver.find_element(By.XPATH, '/html/body/div[5]/div[2]/main/div/article/div/div/div[1]/div/ss3-force-full-width/div/div[1]/div/div/div/div[2]/div[2]/img').click()
         #left_arrow_key = driver.find_element(By.XPATH, '/html/body/div[5]/div[2]/main/div/article/div/div/div[1]/div/ss3-force-full-width/div/div[1]/div/div/div/div[2]/div[4]/img').click()
         faculty_and_course_button = driver.find_element(By.XPATH, '/html/body/div[5]/div[2]/main/div/article/div/div/div[4]/div/a/div/div[1]/picture/img').click()

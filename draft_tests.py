4ddf9a81d981b71fd33a39769ccc4ebcd9d2a207
Jeffrey-Chung <Cheuk.Chung@uts.edu.au>
2023-03-24 15:57:15 +1100

    Loop through all dots and click on them

--- draft_tests.py
+++ draft_tests.py
@@ -34,8 +34,9 @@ def ui_test(driver):
         else:
                 print("The website does not have a valid certificate.")
         action_chain = ActionChains(driver)
-        second_dot = driver.find_element(By.XPATH, '/html/body/div[5]/div[2]/main/div/article/div/div/div[1]/div/ss3-force-full-width/div/div[1]/div/div/div/div[2]/div[3]/div/div[2]/div').click()
-        first_dot = driver.find_element(By.XPATH, '/html/body/div[5]/div[2]/main/div/article/div/div/div[1]/div/ss3-force-full-width/div/div[1]/div/div/div/div[2]/div[3]/div/div[1]/div').click()
+        #Arrow keys are not visible in google chrome
+        for dot_numbers in range(5):
+                driver.find_element(By.XPATH, f'/html/body/div[5]/div[2]/main/div/article/div/div/div[1]/div/ss3-force-full-width/div/div[1]/div/div/div/div[2]/div[3]/div/div[{dot_numbers+1}]/div').click()
         video = driver.find_element(By.XPATH, '/html/body/div/div[1]')
         action_chain.move_to_element(video).perform()
         faculty_and_course_button = driver.find_element(By.XPATH, '/html/body/div[5]/div[2]/main/div/article/div/div/div[4]/div/a/div/div[1]/picture/img').click()
--- test_uts_chinese_website.py
+++ test_uts_chinese_website.py
@@ -52,8 +52,9 @@ def ui_test(driver):
                 print("The website does not have a valid certificate.")
         action_chain = ActionChains(driver)
         #Arrow keys are not visible in google chrome
-        second_dot = driver.find_element(By.XPATH, '/html/body/div[5]/div[2]/main/div/article/div/div/div[1]/div/ss3-force-full-width/div/div[1]/div/div/div/div[2]/div[3]/div/div[2]/div').click()
-        first_dot = driver.find_element(By.XPATH, '/html/body/div[5]/div[2]/main/div/article/div/div/div[1]/div/ss3-force-full-width/div/div[1]/div/div/div/div[2]/div[3]/div/div[1]/div').click()
+        #Loop through all the blue dots
+        for dot_numbers in range(5):
+                driver.find_element(By.XPATH, f'/html/body/div[5]/div[2]/main/div/article/div/div/div[1]/div/ss3-force-full-width/div/div[1]/div/div/div/div[2]/div[3]/div/div[{dot_numbers+1}]/div').click()
         video = driver.find_element(By.XPATH, '/html/body/div/div[1]')
         action_chain.move_to_element(video).perform()
         faculty_and_course_button = driver.find_element(By.XPATH, '/html/body/div[5]/div[2]/main/div/article/div/div/div[4]/div/a/div/div[1]/picture/img').click()
@@ -61,5 +62,5 @@ def ui_test(driver):
 
 if __name__ == "__main__":
         ui_test(firefox_driver)
-        #ui_test(chrome_driver)
+        ui_test(chrome_driver)
         ui_test(edge_driver)

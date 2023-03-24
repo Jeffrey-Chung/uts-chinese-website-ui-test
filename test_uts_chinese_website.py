961fc582d9a449a6d1f536406c0890aa159944e0
Jeffrey-Chung <Cheuk.Chung@uts.edu.au>
2023-03-24 15:36:05 +1100

    Found the correct ID for the left and right arrow keys

--- test_uts_chinese_website.py
+++ test_uts_chinese_website.py
@@ -43,6 +43,7 @@ chrome_driver.get('https://utsaustralia.cn/')
 edge_driver.get('https://utsaustralia.cn/')  
  
 
+# Run deez tests!
 def ui_test(driver):
         #Check if the website has a valid certificate by starting with http://
         if driver.current_url.startswith('https://'):
@@ -50,12 +51,11 @@ def ui_test(driver):
         else:
                 print("The website does not have a valid certificate.")
         action_chain = ActionChains(driver)
-        '''For whatever reason, github action is not able to find the right and left arrow key element'''
-        #right_arrow_key = driver.find_element(By.XPATH, '/html/body/div[5]/div[2]/main/div/article/div/div/div[1]/div/ss3-force-full-width/div/div[1]/div/div/div/div[2]/div[2]/img').click()
-        #left_arrow_key = driver.find_element(By.XPATH, '/html/body/div[5]/div[2]/main/div/article/div/div/div[1]/div/ss3-force-full-width/div/div[1]/div/div/div/div[2]/div[4]/img').click()
-        faculty_and_course_button = driver.find_element(By.XPATH, '/html/body/div[5]/div[2]/main/div/article/div/div/div[4]/div/a/div/div[1]/picture/img').click()
+        right_arrow_key = driver.find_element(By.XPATH, '//*[@id="n2-ss-20-arrow-next"]').click()
+        left_arrow_key = driver.find_element(By.XPATH, '//*[@id="n2-ss-20-arrow-previous"]').click()
         video = driver.find_element(By.XPATH, '/html/body/div/div[1]')
         action_chain.move_to_element(video).perform()
+        faculty_and_course_button = driver.find_element(By.XPATH, '/html/body/div[5]/div[2]/main/div/article/div/div/div[4]/div/a/div/div[1]/picture/img').click()
         driver.quit()
 
 if __name__ == "__main__":

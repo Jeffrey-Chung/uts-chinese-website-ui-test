a084262952ab5d7ccc742086b2dd7a2928850f94
Jeffrey-Chung <Cheuk.Chung@uts.edu.au>
2023-03-27 13:04:02 +1100

    Explictly wait for all survey buttons before clicking
    This will make it also work on github actions

--- draft_tests_uts_chinese_website.py
+++ draft_tests_uts_chinese_website.py
@@ -71,11 +71,17 @@ def ui_test(driver):
                 #action_chain.move_to_element(contact_number).send_keys("0432 995 543").perform()
                 email = driver.find_element(By.XPATH, '/html/body/div[5]/div[2]/main/div/article/div/div/div[1]/div[1]/div/form/div[2]/div[3]/span/input').send_keys("svcbnmjhtgfd@gmail.com")
                 #action_chain.move_to_element(email).send_keys("svcbnmjhtgfd@gmail.com").perform()
-                school_year_dropdown_bar = driver.find_element(By.XPATH, '/html/body/div[5]/div[2]/main/div/article/div/div/div[1]/div[1]/div/form/div[2]/div[4]/span/select').click()
-                #action_chain.move_to_element(school_year_dropdown_bar).click().perform()
-                year_2_button = driver.find_element(By.XPATH, f'/html/body/div[5]/div[2]/main/div/article/div/div/div[1]/div[1]/div/form/div[2]/div[4]/span/select/option[2]').click()
-                #action_chain.move_to_element(year_2_button).click().perform()
                 '''
+                school_year_dropdown_bar = WebDriverWait(driver, 10).until(
+                EC.element_to_be_clickable((By.XPATH, '/html/body/div[5]/div[2]/main/div/article/div/div/div[1]/div[1]/div/form/div[2]/div[4]/span/select'))
+        )
+                action_chain.move_to_element(school_year_dropdown_bar).click().perform()
+                year_2_button = WebDriverWait(driver, 10).until(
+                EC.element_to_be_clickable((By.XPATH, f'/html/body/div[5]/div[2]/main/div/article/div/div/div[1]/div[1]/div/form/div[2]/div[4]/span/select/option[2]'))
+        )
+                action_chain.move_to_element(year_2_button).click().perform()
+
+                
 
         finally:
                 driver.quit()

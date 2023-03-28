3d4f32e27ba21c7e9812c87412be5196a687d0c4
Jeffrey-Chung <Cheuk.Chung@uts.edu.au>
2023-03-28 11:05:59 +1100

    Convert fill in survey code to a function

--- draft_tests_uts_chinese_website.py
+++ draft_tests_uts_chinese_website.py
@@ -23,9 +23,39 @@ command_executor="http://localhost:4444",
 options=firefox_options
 )
 
-
-
 firefox_driver.get('https://utsaustralia.cn/') #Fire up the Fox Cannon!
+
+def fill_survey(driver):
+                action_chain = ActionChains(driver)
+                        #Fills in the details of the contact form, each variable represents each field
+                '''This code onwards can only be run in non-headless mode'''
+                name = WebDriverWait(driver, 20).until(
+                EC.element_to_be_clickable((By.XPATH, '/html/body/div[5]/div[2]/main/div/article/div/div/div[1]/div[1]/div/form/div[2]/div[1]/span/input'))
+        ).send_keys("Jane Smith")
+                #action_chain.move_to_element(name).send_keys("Jane Smith").perform()
+                
+                contact_number = WebDriverWait(driver, 10).until(
+                EC.element_to_be_clickable((By.XPATH, '/html/body/div[5]/div[2]/main/div/article/div/div/div[1]/div[1]/div/form/div[2]/div[2]/span/input'))
+        ).send_keys("0432 995 543")
+                #action_chain.move_to_element(contact_number).send_keys("0432 995 543").perform()
+                email = WebDriverWait(driver, 10).until(
+                EC.element_to_be_clickable((By.XPATH, '/html/body/div[5]/div[2]/main/div/article/div/div/div[1]/div[1]/div/form/div[2]/div[3]/span/input'))
+        ).send_keys("svcbnmjhtgfd@gmail.com")
+                #action_chain.move_to_element(email).send_keys("svcbnmjhtgfd@gmail.com").perform()
+                
+                enquiry_text = WebDriverWait(driver, 10).until(
+                EC.element_to_be_clickable((By.XPATH, '/html/body/div[5]/div[2]/main/div/article/div/div/div[1]/div[1]/div/form/div[2]/div[9]/span/textarea'))
+        ).send_keys("THIS IS JUST FOR TESTING")
+                
+                captcha = WebDriverWait(driver, 10).until(
+                EC.element_to_be_clickable((By.XPATH, '//html/body/div[5]/div[2]/main/div/article/div/div/div[1]/div[1]/div/form/div[2]/div[10]/span/input'))
+        ).send_keys("1234")
+                
+                age_checkbox = WebDriverWait(driver, 10).until(
+                EC.element_to_be_clickable((By.XPATH, '/html/body/div[5]/div[2]/main/div/article/div/div/div[1]/div[1]/div/form/div[2]/div[12]/span/span/span/label/input'))
+        )
+                action_chain.move_to_element(age_checkbox).click().perform()
+
 
 # Run deez tests!
 def ui_test(driver):
@@ -63,37 +93,9 @@ def ui_test(driver):
                 EC.element_to_be_clickable((By.XPATH, '/html/body/div[5]/div[2]/main/div/article/div/div/div[1]/div/div/div[2]/a[1]'))
         ).click()
                #action_chain.move_to_element(contact_form_button).click().perform()
+                fill_survey(driver)
         
-                #Fills in the details of the contact form, each variable represents each field
-                '''This code onwards can only be run in non-headless mode'''
-                name = WebDriverWait(driver, 20).until(
-                EC.element_to_be_clickable((By.XPATH, '/html/body/div[5]/div[2]/main/div/article/div/div/div[1]/div[1]/div/form/div[2]/div[1]/span/input'))
-        ).send_keys("Jane Smith")
-                #action_chain.move_to_element(name).send_keys("Jane Smith").perform()
-                
-                contact_number = WebDriverWait(driver, 10).until(
-                EC.element_to_be_clickable((By.XPATH, '/html/body/div[5]/div[2]/main/div/article/div/div/div[1]/div[1]/div/form/div[2]/div[2]/span/input'))
-        ).send_keys("0432 995 543")
-                #action_chain.move_to_element(contact_number).send_keys("0432 995 543").perform()
-                email = WebDriverWait(driver, 10).until(
-                EC.element_to_be_clickable((By.XPATH, '/html/body/div[5]/div[2]/main/div/article/div/div/div[1]/div[1]/div/form/div[2]/div[3]/span/input'))
-        ).send_keys("svcbnmjhtgfd@gmail.com")
-                #action_chain.move_to_element(email).send_keys("svcbnmjhtgfd@gmail.com").perform()
-                
-                enquiry_text = WebDriverWait(driver, 10).until(
-                EC.element_to_be_clickable((By.XPATH, '/html/body/div[5]/div[2]/main/div/article/div/div/div[1]/div[1]/div/form/div[2]/div[9]/span/textarea'))
-        ).send_keys("THIS IS JUST FOR TESTING")
-                
-                captcha = WebDriverWait(driver, 10).until(
-                EC.element_to_be_clickable((By.XPATH, '//html/body/div[5]/div[2]/main/div/article/div/div/div[1]/div[1]/div/form/div[2]/div[10]/span/input'))
-        ).send_keys("1234")
-                
-                marketing_checkbox = WebDriverWait(driver, 10).until(
-                EC.element_to_be_clickable((By.XPATH, '/html/body/div[5]/div[2]/main/div/article/div/div/div[1]/div[1]/div/form/div[2]/div[11]/span/span/span/label/input'))
-        ).click()
-                age_checkbox = WebDriverWait(driver, 10).until(
-                EC.element_to_be_clickable((By.XPATH, '/html/body/div[5]/div[2]/main/div/article/div/div/div[1]/div[1]/div/form/div[2]/div[12]/span/span/span/label/input'))
-        ).click()
+
 
         finally:
                 print(driver.current_url)

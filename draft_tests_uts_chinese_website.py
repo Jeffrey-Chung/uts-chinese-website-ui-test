f1650ed830fa72adbd9a29696cf86a16360cb680
Jeffrey-Chung <Cheuk.Chung@uts.edu.au>
2023-03-31 10:36:33 +1100

    Finish off dropdown bar clicking

--- draft_tests_uts_chinese_website.py
+++ draft_tests_uts_chinese_website.py
@@ -43,7 +43,10 @@ def fill_survey(driver):
                 EC.element_to_be_clickable((By.XPATH, '/html/body/div[5]/div[2]/main/div/article/div/div/div[1]/div[1]/div/form/div[2]/div[3]/span/input'))
         ).send_keys("svcbnmjhtgfd@gmail.com")
                 #action_chain.move_to_element(email).send_keys("svcbnmjhtgfd@gmail.com").perform()
-                dropdown_bar = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[5]/div[2]/main/div/article/div/div/div[1]/div[1]/div/form/div[2]/div[4]/span/select/option[2]"))).click()
+
+                year_2_option = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[5]/div[2]/main/div/article/div/div/div[1]/div[1]/div/form/div[2]/div[4]/span/select/option[2]"))).click()
+
+                language_course = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[5]/div[2]/main/div/article/div/div/div[1]/div[1]/div/form/div[2]/div[6]/span/select/option[2]"))).click()
 
                 enquiry_text = WebDriverWait(driver, 10).until(
                 EC.element_to_be_clickable((By.XPATH, '/html/body/div[5]/div[2]/main/div/article/div/div/div[1]/div[1]/div/form/div[2]/div[9]/span/textarea'))
@@ -102,8 +105,7 @@ def ui_test(driver):
                 driver.back()
                 world_rankings_button = driver.find_element(By.XPATH, '/html/body/div[5]/div[2]/main/div/article/div/div/div[5]/div/a/div/div[1]/picture/img').click()         
                 #Clicks on the side bars at the top of each page
-                for side_bars in range(5):
-                        driver.find_element(By.XPATH, f'/html/body/div[4]/header/div/div/nav/ul/li[{side_bars+1}]/a').click()
+                news_sidebar = driver.find_element(By.XPATH, "/html/body/div[4]/header/div/div/nav/ul/li[6]/a").click()
 
 
 

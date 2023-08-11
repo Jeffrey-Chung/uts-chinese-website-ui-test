6089b15b72b79f87f31fb267b3f66b766c54653b
Jeffrey-Chung <Cheuk.Chung@uts.edu.au>
2023-08-11 14:31:12 +1000

    Update tests for survey

--- src/utilities.py
+++ src/utilities.py
@@ -28,31 +28,37 @@ def fill_survey(driver):
                 action_chain = ActionChains(driver)
                 '''This code onwards can only be run in non-headless mode'''
                 name = WebDriverWait(driver, 20).until(
-                EC.element_to_be_clickable((By.XPATH, '/html/body/div[5]/div[2]/main/div/article/div/div/div[1]/div[1]/div/form/div[2]/div[1]/span/input'))
+                EC.element_to_be_clickable((By.XPATH, '/html/body/div[5]/div[2]/main/div/article/div/div/div[2]/form/div[2]/div[1]/span/input'))
         ).send_keys("Jane Smith")
                 #action_chain.move_to_element(name).send_keys("Jane Smith").perform()
                 
                 contact_number = WebDriverWait(driver, 10).until(
-                EC.element_to_be_clickable((By.XPATH, '/html/body/div[5]/div[2]/main/div/article/div/div/div[1]/div[1]/div/form/div[2]/div[2]/span/input'))
+                EC.element_to_be_clickable((By.XPATH, '/html/body/div[5]/div[2]/main/div/article/div/div/div[2]/form/div[2]/div[3]/span/input'))
         ).send_keys("0432 995 543")
                 #action_chain.move_to_element(contact_number).send_keys("0432 995 543").perform()
                 email = WebDriverWait(driver, 10).until(
-                EC.element_to_be_clickable((By.XPATH, '/html/body/div[5]/div[2]/main/div/article/div/div/div[1]/div[1]/div/form/div[2]/div[3]/span/input'))
+                EC.element_to_be_clickable((By.XPATH, '/html/body/div[5]/div[2]/main/div/article/div/div/div[2]/form/div[2]/div[2]/span/input'))
         ).send_keys("svcbnmjhtgfd@gmail.com")
                 #action_chain.move_to_element(email).send_keys("svcbnmjhtgfd@gmail.com").perform()
 
-                year_2_option = WebDriverWait(driver, 60).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[5]/div[2]/main/div/article/div/div/div[1]/div[1]/div/form/div[2]/div[4]/span/select/option[2]"))).click()
+                subject_interest = WebDriverWait(driver, 60).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[5]/div[2]/main/div/article/div/div/div[2]/form/div[3]/div[1]/span/select/option[2]"))).click()
+
+                when_to_start = WebDriverWait(driver, 60).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[5]/div[2]/main/div/article/div/div/div[2]/form/div[3]/div[2]/span/select/option[7]"))).click()
+
+                i_am_parent = WebDriverWait(driver, 60).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[5]/div[2]/main/div/article/div/div/div[2]/form/div[3]/div[3]/span/select/option[5]"))).click()
+
+                am_i_eligible = WebDriverWait(driver, 60).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[5]/div[2]/main/div/article/div/div/div[2]/form/div[5]/div[3]/span/select/option[2]"))).click()
 
                 enquiry_text = WebDriverWait(driver, 10).until(
-                EC.element_to_be_clickable((By.XPATH, '/html/body/div[5]/div[2]/main/div/article/div/div/div[1]/div[1]/div/form/div[2]/div[9]/span/textarea'))
+                EC.element_to_be_clickable((By.XPATH, '/html/body/div[5]/div[2]/main/div/article/div/div/div[2]/form/div[5]/div[4]/span/textarea'))
         ).send_keys("THIS IS JUST FOR TESTING")
                 
                 captcha = WebDriverWait(driver, 10).until(
-                EC.element_to_be_clickable((By.XPATH, '//html/body/div[5]/div[2]/main/div/article/div/div/div[1]/div[1]/div/form/div[2]/div[10]/span/input'))
+                EC.element_to_be_clickable((By.XPATH, '/html/body/div[5]/div[2]/main/div/article/div/div/div[2]/form/div[5]/div[6]/span/input'))
         ).send_keys("1234")
                 
                 age_checkbox = WebDriverWait(driver, 20).until(
-                EC.element_to_be_clickable((By.XPATH, '/html/body/div[5]/div[2]/main/div/article/div/div/div[1]/div[1]/div/form/div[2]/div[12]/span/span/span/label/input'))
+                EC.element_to_be_clickable((By.XPATH, '/html/body/div[5]/div[2]/main/div/article/div/div/div[2]/form/div[5]/div[5]/span/span/span/label/input'))
         )
                 driver.execute_script("arguments[0].click();", age_checkbox)
                 driver.back()
@@ -70,15 +76,12 @@ def ui_test(driver):
                         print("The website does not have a valid certificate.")
                 action_chain = ActionChains(driver)
         
-                #Page may take a long time to fully load and render, if it takes > 2mins it will throw an error
-                driver.implicitly_wait(120)
+
                 #Arrow keys are not visible in google chrome
                 for dot_numbers in range(1, 5):
                         address = f"/html/body/div[5]/div[2]/main/div/article/div/div/div[1]/ss3-force-full-width/div/div[1]/div/div/div/div[2]/div[3]/div/div[{dot_numbers}]/div"
                         circle_icon = driver.find_element(By.XPATH, address)
                         driver.execute_script("arguments[0].click();", circle_icon)
-                        
-                
                         
                 
                 #This will hover to the video area but won't do anything
@@ -92,10 +95,8 @@ def ui_test(driver):
                 action_chain.move_to_element(play_button).click().perform()
                 '''
                 
-                faculty_and_course_button = driver.find_element(By.XPATH, '/html/body/div[5]/div[2]/main/div/article/div/div/div[4]/div/a/div/div[1]/picture/img').click()
-                
-                #Page may take a long time to fully load and render, if it takes > 2mins it will throw an error
-                driver.implicitly_wait(120)
+                faculty_and_course_button = driver.find_element(By.XPATH, '/html/body/div[5]/div[2]/main/div/article/div/div/div[5]/div/a/div/div[1]/picture/img').click()
+
                 contact_form_button = WebDriverWait(driver, 30).until(
                 EC.element_to_be_clickable((By.XPATH, '/html/body/div[5]/div[2]/main/div/article/div/div/div[1]/div/div/div[2]/a[1]'))
         ).click()
